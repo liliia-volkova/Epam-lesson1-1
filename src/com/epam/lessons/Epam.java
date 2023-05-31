cb45f4eb64a0dcb2c0a7ecc64b51d0dc715c350c
Liliia Volkova <volkol01@kits.kfplc.com>
2023-05-31 19:54:52 +0300

    messagegg

--- src/com/epam/lessons/Epam.java
+++ src/com/epam/lessons/Epam.java
@@ -4,7 +4,7 @@ public class Epam {
     public static void main(String[] args) {
         System.out.println("**** ****     *      *   ********88888***%");
         System.out.println("*    *  *    * *     ** **$");
-        System.out.println("**   ****   *   *    * * *#");
+        System.out.println("**   **gfhhh**   *   *    * * *#");
         System.out.println("*    *     *******   *   *@");
         System.out.println("**** *    *       *  *   *&");
 
