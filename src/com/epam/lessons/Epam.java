caaaa13f4dcde2ecaa7ca3f163181ab14f5b3788
Liliia Volkova <volkol01@kits.kfplc.com>
2023-05-31 18:25:01 +0300

    Message-1

--- src/com/epam/lessons/Epam.java
+++ src/com/epam/lessons/Epam.java
@@ -2,7 +2,7 @@ package com.epam.lessons;
 
 public class Epam {
     public static void main(String[] args) {
-        System.out.println("**** ****     *      *   *%");
+        System.out.println("**** ****     *      *   ***********%");
         System.out.println("*    *  *    * *     ** **$");
         System.out.println("**   ****   *   *    * * *#");
         System.out.println("*    *     *******   *   *@");
